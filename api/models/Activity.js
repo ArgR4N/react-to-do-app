0e4ce9cbcc40fc7eb459046ce0b459e97db9f934
r4n <ramiroreinaldo12@gmail.com>
2023-06-22 23:14:42 -0300

    ❌ Deleted old react-only files

--- api/models/Activity.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AtivitySchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  dateFor: { type: Date, default: Date.now}
-});
-
-const Ativity = mongoose.model('Ativity', AtivitySchema);
-module.exports = Ativity;
